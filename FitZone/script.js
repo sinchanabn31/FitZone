6c65fa71285e22030b860421a574b35c674d3135
Sinchana B N <sinchanabn31@gmail.com>
2026-07-05 17:59:01 +0530

    Add files via upload

---
+++ FitZone/script.js
@@ -0,0 +1,51 @@
+console.log("FitZone Website Loaded Successfully!");
+function toggleStrength(){
+
+    const info = document.getElementById("strength-info");
+
+    if(info.style.display === "block"){
+
+        info.style.display = "none";
+
+    }
+
+    else{
+
+        info.style.display = "block";
+
+    }
+
+}
+function toggleCardio(){
+
+    const info = document.getElementById("cardio-info");
+
+    if(info.style.display === "block"){
+
+        info.style.display = "none";
+
+    }
+    else{
+
+        info.style.display = "block";
+
+    }
+
+}
+
+function toggleYoga(){
+
+    const info = document.getElementById("yoga-info");
+
+    if(info.style.display === "block"){
+
+        info.style.display = "none";
+
+    }
+    else{
+
+        info.style.display = "block";
+
+    }
+
+}
